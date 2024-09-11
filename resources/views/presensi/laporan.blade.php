7d02c93060bb787ccac687f5361ac5abe0d13fa0
DESKTOP-9VJCIF5\nayla <alfarobi695@gmail.com>
2024-09-12 02:01:57 +0700

    up

--- resources/views/presensi/laporan.blade.php
+++ resources/views/presensi/laporan.blade.php
@@ -2,14 +2,14 @@
 @section('content')
 <div class="page-header d-print-none">
     <div class="container-xl">
-      <div class="row g-2 align-items-center">
-        <div class="col">
-          <!-- Page pre-title -->
-          <h2 class="page-title">
-            Laporan Presensi
-          </h2>
+        <div class="row g-2 align-items-center">
+            <div class="col">
+            <!-- Page pre-title -->
+            <h2 class="page-title">
+                Laporan Presensi
+            </h2>
+            </div>
         </div>
-      </div>
     </div>
 </div>
 
